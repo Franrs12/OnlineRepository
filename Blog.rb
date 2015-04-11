4a3f456c7e6338a75836c38222b3c6bf3fe0faa6
Francisco José Rubiano Sánchez <franrs12@gmail.com>
2015-04-12 01:06:21 +0200

    parece que puede estar bien el blog

--- Blog.rb
+++ Blog.rb
@@ -1,53 +1,60 @@
 #Define una clase
 class Post
-	#constructor method
-	def initialize(t,d,p)
-		@title, @date, @text = t, d, p
-	end
-	
-	#accesor methods
-	def imprimeTitulo
-		@title
-	end
-	
-	def imprimeFecha
-		@date
-	end
-	
-	def imprimeTexto
-		@text
-	end
+    #constructor method
+    def initialize(t,d,p)
+        @title, @date, @text = t, d, p
+    end
+    
+    #accesor methods
+    def imprimeTitulo
+        @title
+    end
+    
+    def imprimeFecha
+        @date
+    end
+    
+    def imprimeTexto
+        @text
+    end
 end
 
 #Define un método
 #def Blog
-#	Post.each {|post| puts "#{post.title}#{post.date}" + puts "******" + puts "#{post.text}" + "--------"}
+#    Post.each {|post| puts "#{post.title}#{post.date}" + puts "******" + puts "#{post.text}" + "--------"}
 #end
 
 
 #Crea el método del blog
 def Blog
-	#Crea un objeto
-	miPrimerPost = Post.new("Mi Primer Post", "11-4-2015", "Este es el texto de mi primer post.")
-	miSegundoPost = Post.new("Mi Segundo Post", "11-4-2015 por la tarde", "Este es otro texto.")
+    #Crea un objeto
+    post1 = Post.new("Mi Primer Post", "11-4-2015", "Este es el texto de mi primer post.")
+    post2 = Post.new("Mi Segundo Post", "11-4-2015 por la tarde", "Este es otro texto.")
+    post3 = Post.new("Tercer post", "12-4-2015", "El texto del tercer post es bastante más largo que el de los otros dos anteriores porque me he enrollado más.")
+    
+    #Usa accesor methods
+   	x1 = post1.imprimeTitulo()
+	y1 = post1.imprimeFecha()
+	z1 = post1.imprimeTexto()
+	x2 = post2.imprimeTitulo()
+	y2 = post2.imprimeFecha()
+	z2 = post2.imprimeTexto()
+	x3 = post3.imprimeTitulo()
+	y3 = post3.imprimeFecha()
+	z3 = post3.imprimeTexto()
 
-	
-	#Usa accesor methods
-	x = miPrimerPost.imprimeTitulo()
-	y = miPrimerPost.imprimeFecha()
-	z = miPrimerPost.imprimeTexto()
-	x2 = miSegundoPost.imprimeTitulo()
-	y2 = miSegundoPost.imprimeFecha()
-	z2 = miSegundoPost.imprimeTexto()
-
-	puts "#{x} #{y}"
-	puts "********"
-	puts "#{z}"
-	puts "--------"
-	puts "#{x2} #{y2}"
-	puts "********"
-	puts "#{z2}"
-	puts "--------"
+    puts "#{x1} #{y1}"
+    puts "********"
+    puts "#{z1}"
+    puts "--------"
+    puts "#{x2} #{y2}"
+    puts "********"
+    puts "#{z2}"
+    puts "--------"
+    puts "#{x3} #{y3}"
+    puts "********"
+    puts "#{z3}"
+    puts "--------"
 end
 
 Blog()
