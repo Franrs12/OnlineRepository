9aa092530ad86c031ad53c1858f9ad50279fce54
Francisco José Rubiano Sánchez <franrs12@gmail.com>
2015-04-16 17:53:18 +0200

    parece que ya funciona el blog

---
+++ Week 2/RAILS_BLOG/blog/db/seeds.rb
@@ -0,0 +1,8 @@
+Article.create( title: "My first article", text: "Welcome to my blog!")
+Article.create( title: "Learning Rails", text: "Rails is awesome!")
+     #creates add an article to the database
+Article.create(title: "Los saltamontes", text: "Los caelíferos o celíferos (Caelifera) son un suborden del orden 
+Orthoptera, comúnmente conocidos como saltamontes, chapulines o langostas. Se diferencian del otro suborden 
+de ortópteros (Ensifera) porque las antenas son cortas y tienen casi siempre menos de 30 artejos. Resultan 
+un manjar para todos los asiáticos. Se estima que la separación de los dos subórdenes se produjo en el límite 
+Permo-Triásico. Se trata de insectos herbívoros, algunos de los cuales son graves plagas para la agricultura.")
